6d09d00115fc328a45c32c0945504d35793db10e
budzikovy <115116202+budzikovy@users.noreply.github.com>
2025-04-11 15:07:44 +0200

    Add files via upload

---
+++ src/main/java/tftcalculator/tftcalculator/TftcalculatorApplication.java
@@ -0,0 +1,13 @@
+package tftcalculator.tftcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TftcalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tftcalculator/tftcalculator/TftcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package tftcalculator.tftcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TftcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
